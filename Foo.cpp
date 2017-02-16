2740c7a46f647954b28c5d9b37a340e60c2fabb3
jekajeka80 <ealexanov@gmail.com>
2017-02-16 18:33:34 +0400

    Add files via upload

---
+++ Foo.cpp
@@ -0,0 +1,5 @@
+#include <string>
+
+std::string bar() {
+	return "Hello world"; }
+
---
+++ appFoo.cpp
@@ -0,0 +1,8 @@
+#include "libFoo.h"
+#include <iostream>
+
+
+int main(int argc, char** argv){
+	std::cout << bar() << std::endl;
+	return 0;
+}
